e830f1e2e70560ffe84075a0947401f6c2fb19d2
jamwoods <jamwoods@fullsail.edu>
2014-07-14 11:01:54 -0600

    Almost done with Sparky
    Syntax for pulling from prompt box almost complete

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -5,13 +5,15 @@
  */
 
 
-var humanYears = prompt("How old is your dog in human years?");
-if (humanYears != null) {
-    document.getElementById("demo").innerHTML =
-        "Hello " + person + "! How are you today?";
-}
+//var humanYears = prompt("How old is your dog in human years?");
+//if (humanYears != null) {
+   // document.getElementById("demo").innerHTML =
+       // "Hello " + person + "! How are you today?";
+//}
 //alert("How old is your dog in human years?");//
-var humanYears = prompt
+
+var agePrompt =  prompt("How old is Sparky in human years?","0");
+var humanYears = parseInt(agePrompt);
 var dogYears = humanYears * 7;
 console.log(dogYears);
 
