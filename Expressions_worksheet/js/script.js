54de5c1a866af724db447e21c5f1756ef8af55cd
jamwoods <jamwoods@fullsail.edu>
2014-07-14 11:22:59 -0600

    Updated verbiage to follow exercise instructions
    Updated verbiage to follow exercise instructions

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -10,4 +10,5 @@ var humanYears = parseInt(agePrompt); //Setting humanYears variable equal to val
 var dogYears = humanYears * 7; //Calculating dog years by multiplying value in humanYears by 7
 console.log(dogYears);
 
-alert("Sparky is" + " " +  (dogYears) + " " +  "year old in dog years"); //Alert showing final calculation with friendly verbiage.
+//alert("Sparky is" + " " +  (dogYears) + " " +  "year old in dog years"); //Alert showing final calculation with friendly verbiage.
+alert("Sparky is" + " " +  (humanYears) + " " +  "human years old which is" + " " +  (dogYears) + " " + "in dog years");
