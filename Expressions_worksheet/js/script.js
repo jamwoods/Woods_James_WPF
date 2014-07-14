8fb29138ae5c0aee7e0afa360712048f9b563c5e
jamwoods <jamwoods@fullsail.edu>
2014-07-14 11:18:39 -0600

    Added Comments
    Added Comments

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -4,10 +4,10 @@
  * Expressions_worksheet
  */
 
-
-var agePrompt =  prompt("How old is Sparky in human years?","0");
-var humanYears = parseInt(agePrompt);
-var dogYears = humanYears * 7;
+//Sparkys age calculation.
+var agePrompt =  prompt("How old is Sparky in human years?","0"); //prompt box to gather human years from user
+var humanYears = parseInt(agePrompt); //Setting humanYears variable equal to value obtained from prompt box
+var dogYears = humanYears * 7; //Calculating dog years by multiplying value in humanYears by 7
 console.log(dogYears);
 
-alert("Sparky is" + " " +  (dogYears) + " " +  "year old in dog years");
+alert("Sparky is" + " " +  (dogYears) + " " +  "year old in dog years"); //Alert showing final calculation with friendly verbiage.
