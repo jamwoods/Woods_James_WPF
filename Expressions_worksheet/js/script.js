6c6dd87ee0be7ff635b1c93b9247ecb4a07442b3
jamwoods <jamwoods@fullsail.edu>
2014-07-14 17:19:23 -0400

    Notation Added
    Notation Added

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -14,20 +14,20 @@ console.log(dogYears);
 alert("Sparky is" + " " +  (humanYears) + " " +  "human years old which is" + " " +  (dogYears) + " " + "in dog years");
 
 //Slice of Pie part 1
-var pizzaSlice = 8;
-var peopleCount = 24;
-var pizzaCount = 10;
-var slicesPer = parseFloat(pizzaSlice*pizzaCount/peopleCount)
+var pizzaSlice = 8; //Slices per pizza
+var peopleCount = 24; //Total people at the party
+var pizzaCount = 10; //Total number of pizzas
+var slicesPer = parseFloat(pizzaSlice*pizzaCount/peopleCount) //Slices per person calculation using decimals
 console.log(slicesPer);
 
 //SLice of Pie part 2-How many is Sparky going to get-count only whole pieces.
-var sparkySlice = (slicesPer-3) * 24;
-var sparkySliceRound = parseInt(sparkySlice)
+var sparkySlice = (slicesPer-3) * 24; //Calculation of # of slices Sparky gets. This multiplies the remainder by the number of people.
+var sparkySliceRound = parseInt(sparkySlice) //This calculation removes te decimal. Rounded up.
 console.log(sparkySliceRound);
 
 //Average shopping bill
-var shoppingBills=[80,65,110,89,76];
-var shoppingTotal=shoppingBills[0] + shoppingBills[1] + shoppingBills[2] + shoppingBills[3] + shoppingBills[4];
+var shoppingBills=[80,65,110,89,76]; //An array of each weeks shopping bill.
+var shoppingTotal=shoppingBills[0] + shoppingBills[1] + shoppingBills[2] + shoppingBills[3] + shoppingBills[4]; //Total spent
 console.log(shoppingTotal);
-var shoppingAverage=shoppingTotal/5
+var shoppingAverage=shoppingTotal/5 //Average of each shopping bill
 console.log(shoppingAverage);
