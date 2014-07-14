f5c3720c11230b6a12402bf1e1da7157b9ea632b
jamwoods <jamwoods@fullsail.edu>
2014-07-14 10:52:45 -0600

    First iteration of prompt box
    Researching how to retrieve a value from a prompt box

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -5,9 +5,14 @@
  */
 
 
-var humanYears = 5;
+var humanYears = prompt("How old is your dog in human years?");
+if (humanYears != null) {
+    document.getElementById("demo").innerHTML =
+        "Hello " + person + "! How are you today?";
+}
+//alert("How old is your dog in human years?");//
+var humanYears = prompt
 var dogYears = humanYears * 7;
 console.log(dogYears);
 
-
-alert(dogYears);
+alert("Sparky is" + "" + (dogYears) + ""  + "years old");
