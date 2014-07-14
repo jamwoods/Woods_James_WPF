9557277e3370a52447613156310f1afaa2d05760
jamwoods <jamwoods@fullsail.edu>
2014-07-14 11:13:17 -0600

    Spaces in the string
    Finally figured out that I have to put a space in between the Quotes.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -5,16 +5,9 @@
  */
 
 
-//var humanYears = prompt("How old is your dog in human years?");
-//if (humanYears != null) {
-   // document.getElementById("demo").innerHTML =
-       // "Hello " + person + "! How are you today?";
-//}
-//alert("How old is your dog in human years?");//
-
 var agePrompt =  prompt("How old is Sparky in human years?","0");
 var humanYears = parseInt(agePrompt);
 var dogYears = humanYears * 7;
 console.log(dogYears);
 
-alert("Sparky is" + "" +  (dogYears) + "" +  "year old in dog years");
+alert("Sparky is" + " " +  (dogYears) + " " +  "year old in dog years");
