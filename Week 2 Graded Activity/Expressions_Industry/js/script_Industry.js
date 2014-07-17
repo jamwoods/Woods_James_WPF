45c4f563f390ab3e867d367758c6ca50f13c4af9
jamwoods <jamwoods@fullsail.edu>
2014-07-16 20:42:42 -0400

    Putting it all together
    Putting it all together

--- Week 2 Graded Activity/Expressions_Industry/js/script_Industry.js
+++ Week 2 Graded Activity/Expressions_Industry/js/script_Industry.js
@@ -5,22 +5,45 @@
  */
 
 //Hours worked in a week
-alert ("Let's calculate the number of hours you will be working this week as a Corporate Trainer.");
-alert ("Your total hours are comprised of four primary tasks which are prep, travel, delivery and post training.");
-alert ("The two largest fluctuating variables will be travel destination and class duration");
-alert ("Classes run from 1 day to 5 days and travel will be either to the West Coast, Mid West or East Coast");
+//alert ("Let's calculate the number of hours you will be working this week as a Corporate Trainer.");
+//alert ("Your total hours are comprised of four primary tasks which are prep, travel, delivery and post training.");
+//alert ("The two largest fluctuating variables will be travel destination and class duration");
+//alert ("Classes run from 1 day to 5 days and travel will be either to the West Coast, Mid West or East Coast");
 
 var daysPrompt =  prompt("Please choose the number of days you will be training this week.","0"); //prompt box to gather human years from user
 var trainingDays = parseInt(daysPrompt); //Setting humanYears variable equal to value obtained from prompt box
+var totalDeliveryHours = trainingDays * 8
+console.log(totalDeliveryHours);
 
 var locationPrompt =  prompt("Please input your destination as either W, MW or E","Select Location"); //prompt box to gather human years from user
 var locationCase = locationPrompt.toUpperCase();
 console.log(locationCase);
+if (locationCase == "W")
+{var locationTime = 2;}
+if (locationCase == "MW")
+{var locationTime = 4;}
+if (locationCase == "E")
+{var locationTime = 6;}
+console.log(locationTime);
+
 
 alert ("How prepared are you for class? We will factor in the number of times you have taught the class in order to determine prep time");
 var classExp =  prompt("Please enter the number of times you have taught this class.","0"); //prompt box to
-var prepHours = parseInt(classExp); //Setting humanYears variable equal to value obtained from prompt box
-console.log(prepHours);
+var prep = parseInt(classExp); //Setting humanYears variable equal to value obtained from prompt box
+console.log(prep);
+
+if (prep >= 0 && prep <= 3)
+{var prepTime = 8;}
+if (prep >= 4 && prep <= 8)
+{var prepTime = 4;}
+if (prep >= 9)
+{var prepTime = 1;}
+console.log(prepTime);
+
+
+
+
+//alert("Your total working hours for this week include" + " " +  (product) + " " +  "was originally" + " " + "$"+(price) + " " + "but after a" + " " + (discountPercent)+"%" + " " +  "discount it is now" + " " + "$"+ (discountedPrice) + " " + "without tax, and" + " " + "$"+(totalTotal) + " " + "with tax");
 
 /*
 var dogYears = humanYears * 7; //Calculating dog years by multiplying value in humanYears by 7
