433ca3aadaffe4e1b873578d0a0a580968089873
jamwoods <jamwoods@fullsail.edu>
2014-07-20 17:13:00 -0600

    Exact match conditional
    Exact match conditional

--- Week 3 Non_Graded_Activites/Conditional Practice/js/conditional_script.js
+++ Week 3 Non_Graded_Activites/Conditional Practice/js/conditional_script.js
@@ -4,3 +4,13 @@
  * Week 3 Conditional Practice
  */
 
+//Conditional Comparison Statement with Temp
+
+//Populated value in var determines whether the subsequent argument will execute.
+var temp = 82;
+if (temp>70) {goToTheBeach();
+}
+
+var temp = 82;//In this scenario, you are trying to compare values and look for an exact match
+if (temp==70) {goToTheBeach();
+}
