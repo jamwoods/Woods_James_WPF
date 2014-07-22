5a686711cb0864faf2abc1841aabc21fd7510a6c
jamwoods <jamwoods@fullsail.edu>
2014-07-21 20:22:00 -0600

    Last Chance for Gas
    Figuring out formula to determine how many miles the drive can get.

--- Week 3 Non_Graded_Activites/Conditional Worksheet/Group 1/js/conditionalWorksheet_group1.js
+++ Week 3 Non_Graded_Activites/Conditional Worksheet/Group 1/js/conditionalWorksheet_group1.js
@@ -1,60 +1,43 @@
 /**
  * Created by James Woods
  * Origination Date 7/21/14.
- * Week 3 Conditional Worksheet
+ * Week 3 Conditional Worksheet-Group 1
  */
 
-//Conditional Comparison Statement with Temp
+/*Last Chance for Gas
+Given:
+Gas efficiency of the car (in mpg)
+Gauge reading of the gas tank (in %)
+Car’s gas tank capacity (in gallons)
+[JW]-The question really is, can you make it the next 200 miles. The formula for the calculation is amount of gas left, e.g.(25, 50, 75%)
+translated into gallons / mpg. if I have 50% left and I have a 12 gallon tank then I have 6 gallons. if I get 32 MPG then I can go 32*6 Miles.
+So the formula is:
 
-/**
- * Created by James Woods
- * Origination Date 7/16/14.
- * Week 2 Graded Assignment_Personal
- */
+(.50 * 12)*32
 
+Result To Print Out:
+“Yes, you can make it without stopping for gas!” or “You only have X gallons of gas in your tank, better get gas now while you can!”*/
 
-alert ("How much are you really spending on gas a year?");
+//Gas tank gauge reading
+var percentLeft =  prompt("How much gas do you have left? Enter a whole number that represents the remaining percentage, e.g. 25, 50 75.","0"); //User inputs gas mileage
+console.log(percentLeft);
+
+var percentLeftCal = percentLeft/100
+console.log(percentLeftCal);
 
 //Gas mileage
 var mpgPrompt =  prompt("Please enter your cars gas mileage.","0"); //User inputs gas mileage
 var mpg = parseInt(mpgPrompt); //Return user selection as a number
 console.log(mpg);
 
-//Miles to work
-var mtwPrompt =  prompt("How many miles away is your office?","0"); //User inputs distance from home to office
-var mtw = parseInt(mtwPrompt); //Return user selection as a number
-var roundTripYearMTW = mtwPrompt * 2 * 5 * 52//Total miles to and from work for the year.
-console.log(roundTripYearMTW);
+//Gas tank capacity
+var tankCapacity =  prompt("How many gallons of gas does your tank hold?","0"); //User inputs gas mileage
+console.log(tankCapacity);
 
-//Weeks vacation
-var vacationPrompt =  prompt("Please enter the number of weeks vacation you will be taking this year.","0"); //User inputs vacation weeks
-var vacation = parseInt(vacationPrompt); //Return user selection as a number
-console.log(vacation);
+//Number of miles you can travel
+var milesLeft = percentLeftCal * tankCapacity/mpg
+alert (milesLeft)
 
-//Adjusted miles to work
-var trueMTW = roundTripYearMTW - vacation * 5 * 48//Pulling out the miles not driven during vacation
-console.log(trueMTW);
-
-//Vacation Miles
-var vacationMilesPrompt =  prompt("How many miles will you drive during your vacation?","0"); //User inputs vacation weeks
-var vacationMiles = parseInt(vacationMilesPrompt); //Return user selection as a number
-console.log(vacationMiles);
-
-//Weekend Miles
-var weekendMilesPrompt =  prompt("How many miles do you average on the weekend?","0"); //User inputs vacation weeks
-var weekendMiles = parseInt(weekendMilesPrompt); //Return user selection as a number
-console.log(weekendMiles);
-
-//Total miles
-var totalMiles= trueMTW + vacationMiles + weekendMiles //Total calculation composed of true miles + vacation miles plus weekend miles
-console.log(totalMiles);
-
-//Current price per gallon
-var pricePerGallonsPrompt =  prompt("What is the local price per gallon of your gas of choice?","0"); //User inputs vacation weeks
-var pricePerGallon = parseFloat(pricePerGallonsPrompt); //Return user selection as a number
-console.log(pricePerGallon);
-
-//Total yearly cost
 
 var totalYearlyCost = parseFloat(trueMTW/mpg * pricePerGallon + vacationMiles/mpg * pricePerGallon + weekendMiles/mpg * pricePerGallon).toFixed(2);
 console.log(totalYearlyCost); //Calculating total yearly cost so last statement for the alert is easier to manage. Could also have calculated the other three statement and place in separate variable but did not want to.
