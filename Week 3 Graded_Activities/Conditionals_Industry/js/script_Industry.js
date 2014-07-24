7c61e64ee33004eb54894ea6a17d72f66fcea478
jamwoods <jamwoods@fullsail.edu>
2014-07-23 21:33:42 -0600

    Added Comments

--- Week 3 Graded_Activities/Conditionals_Industry/js/script_Industry.js
+++ Week 3 Graded_Activities/Conditionals_Industry/js/script_Industry.js
@@ -12,20 +12,21 @@ alert ("Your bonus is comprised of two elements, a quality score and a utilizati
 //Quality Score
 var qualityPrompt =  prompt("What is your overall quality score for the quarter?"); //User is prompted for their quarterly quality score
 if (qualityPrompt === ""){
-    qualityPrompt = prompt("What is your overall quality score for the quarter? Do not leave blank.","0");
+    qualityPrompt = prompt("What is your overall quality score for the quarter? Do not leave blank.","0");//Re-prompting if no value is entered
 }
 var quality = parseFloat(qualityPrompt); //The number from the prompt is returned with a decimal place if necessary, e.g. 9.2
 console.log(quality);
 
 
 //Utilization
-var utilPrompt =  prompt("What is your average utilization percentage for the quarter?"); //User selects class delivery length
+var utilPrompt =  prompt("What is your average utilization percentage for the quarter?"); //User is prompted for their utilization percentage
 if (utilPrompt === ""){
-    utilPrompt = prompt("What is your overall quality score for the quarter? Do not leave blank");
+    utilPrompt = prompt("What is your overall quality score for the quarter? Do not leave blank");//Re-prompting if no value is entered
 }
-var util = utilPrompt; //Return user selection as a number
+var util = utilPrompt; //Return user selection as a number in this variable
 console.log(util);
 
+//Payout percentage calculation for quality score scale
 if (quality < 7.0)
     {var qPayout = 0};
 if (quality <= 8.0 && quality >=7.0)
@@ -37,6 +38,7 @@ if (quality >= 9.0)
 
 console.log(qPayout);
 
+//Payout percentage calculation for utilization percentage
 if (util < 40)
 {var uPayout = 0};
 if (util <= 45 && util >=40)
