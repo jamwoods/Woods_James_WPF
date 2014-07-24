77f38c42687459f8e243e2bff6da9f9c73d625eb
jamwoods <jamwoods@fullsail.edu>
2014-07-23 19:32:08 -0600

    Complete worksheet group 3
    Complete worksheet group 3

--- Week 3 Non_Graded_Activites/Conditional Worksheet/Group 3/js/conditionalWorksheet_group3.js
+++ Week 3 Non_Graded_Activites/Conditional Worksheet/Group 3/js/conditionalWorksheet_group3.js
@@ -4,37 +4,26 @@
  * Week 3 Conditional Worksheet Group 3
  */
 
-    /*Choose 1 from this group
-     Tire Pressure I
+    //Tire Pressure warning
 
-     To meet to maintenance standards a car’s front two tires should have the same pressure and the back two tires should have the same pressure.  But the front tires and the rear tires can have different pressure than each other, so it is not necessary for all four tires’ pressure to be the same. Create a single conditional that would determine if the tires of a given car are up to spec.
+var frontLeftStandard = 32;//To be used if you want to enforce a minimum air pressure
+var frontRightStandard = 32;//To be used if you want to enforce a minimum air pressure
+var rearLeftStandard = 40;//To be used if you want to enforce a minimum air pressure
+var rearRightStandard = 40;//To be used if you want to enforce a minimum air pressure
 
-
-     Given:
-     Pressure for each tire of the car (in psi) in an array.
-     Result To Print Out:
-     “The tires pass spec!” Or  “ Get your tires checked out!” */
-
-
-
-var frontLeftStandard = 32;
-var frontRightStandard = 32;
-var rearLeftStandard = 40;
-var rearRightStandard = 40;
-
-var frontLeft = prompt("What is the pressure of your front left tire");////prompt to collect Username
+var frontLeft = prompt("What is the pressure of your front left tire");//prompt to collect front left tire pressure
  console.log(frontLeft);
 
- var frontRight = prompt("What is the pressure of your front right tire");////prompt to collect Username
+ var frontRight = prompt("What is the pressure of your front right tire");//prompt to collect front right tire pressure
  console.log(frontRight);
 
- var rearLeft = prompt("What is the pressure of your rear left tire");////prompt to collect Username
+ var rearLeft = prompt("What is the pressure of your rear left tire");//prompt to collect rear left tire pressure
  console.log(rearLeft);
 
- var rearRight = prompt("What is the pressure of your rear right tire");////prompt to collect Username
+ var rearRight = prompt("What is the pressure of your rear right tire");//prompt to collect rear right tire pressure
  console.log(rearRight);
 
-var pressureArray = new Array ();
+var pressureArray = new Array ();//merge all selections into an array
 
 pressureArray[0] = frontLeft;
 pressureArray[1] = frontRight;
@@ -43,7 +32,14 @@ pressureArray[3] = rearRight;
 
 console.log(pressureArray);
 
-if (pressureArray[0]>=32 && pressureArray[1]>=32 && pressureArray[2]>=40 && pressureArray[3]>=40) {
+/*if (pressureArray[0]>=32 && pressureArray[1]>=32 && pressureArray[2]>=40 && pressureArray[3]>=40) {//The code if you want to enforce a minimum air pressure per tire standard
+    alert("The tires pass spec!");
+}else
+    alert("Get your tires checked out!");*/
+
+//If you just simply want to enforce pressure equivalence without referring to a particular pressure
+
+if (pressureArray[0] == pressureArray[1] && pressureArray[2] == pressureArray[3]) {//Enforce pressure equivalence between two front and two rear tires before all clear message
     alert("The tires pass spec!");
 }else
     alert("Get your tires checked out!");
