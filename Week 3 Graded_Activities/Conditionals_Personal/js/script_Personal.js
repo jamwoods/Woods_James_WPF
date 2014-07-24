a14b24bd3ebcfd170d9cab314968aa6f92100241
jamwoods <jamwoods@fullsail.edu>
2014-07-24 17:15:28 -0600

    Alert complete but still need another conditional
    Alert complete but still need another conditional

--- Week 3 Graded_Activities/Conditionals_Personal/js/script_Personal.js
+++ Week 3 Graded_Activities/Conditionals_Personal/js/script_Personal.js
@@ -6,6 +6,8 @@
 
 
 var dailyBudget = 100
+var appAvgCost = 7
+var drinkAvgCost = 7
 
 var estAppBill = prompt("How much money are your desired apps going to cost?")
 if (estAppBill === ""){
@@ -28,16 +30,40 @@ var drinkBill=parseInt(estDrinkBill)
 var estDinnerBill= parseInt(appBill+foodBill+drinkBill);
 console.log(estDinnerBill);
 
+if (estDinnerBill<=dailyBudget){
+    alert("Bon Appetite and Cheers");
+}else if(estDinnerBill>dailyBudget) {
+    alert("Time to make some sacrifices");
 
 var loopCounter
 
-for (loopCounter=estDinnerBill; loopCounter >100; loopCounter-=7)
-    console.log(loopCounter);
+    for (loopCounter=estDinnerBill; loopCounter >100; loopCounter-=7)
+        console.log(loopCounter);
 
-Fire a prompt to cut appetizer and if rejected automatically cut a drink.
+var totalLoops = parseInt(estDinnerBill-loopCounter)/7
+    console.log(totalLoops);
+
+alert("You are going to need to cut" + " " + totalLoops + " " + "apps and/or drinks" )
+
+/*var count;
+console.log("loopCounter" + "<br />");
+for(count = estDinnerBill; count > 100; count-=7){
+
+    console.log(count);
+
+    }
 
 
-/*if (estDinnerBill<=dailyBudget){
+   /* var trimApp = prompt("Would you like to remove an app?")
+if (trimApp === ""){
+        trimApp = prompt("Would you like to remove an app?" Please provide and answer.");//Re-prompting if no value is entered
+
+
+
+/***Fire a prompt to cut appetizer and if rejected automatically cut a drink.
+
+
+/* if (estDinnerBill<=dailyBudget){
     alert("Bon Appetite and Cheers");
 }else if(estDinnerBill>dailyBudget) {
  var updatedDinnerBill= estDinnerBill-appBill
@@ -60,10 +86,10 @@ Fire a prompt to cut appetizer and if rejected automatically cut a drink.
 
  }else{
  console.log("Need o grow a little");
-
+}
 
 
 }
-alert("Your yearly cost for gas consists of" + " " +  "$" + (trueMTW/mpg * pricePerGallon).toFixed(2) + " " +  "for your work commute," + " " + "$" + (vacationMiles/mpg* pricePerGallon).toFixed(2) + " " + "for your vacation commute and" + " " + "$" + (weekendMiles/mpg * pricePerGallon).toFixed(2) + " " +  "for you weekend miles for a total yearly cost of" + " " + "$" + (totalYearlyCost));
+//alert("Your yearly cost for gas consists of" + " " +  "$" + (trueMTW/mpg * pricePerGallon).toFixed(2) + " " +  "for your work commute," + " " + "$" + (vacationMiles/mpg* pricePerGallon).toFixed(2) + " " + "for your vacation commute and" + " " + "$" + (weekendMiles/mpg * pricePerGallon).toFixed(2) + " " +  "for you weekend miles for a total yearly cost of" + " " + "$" + (totalYearlyCost));
 
-*/
+*/}
