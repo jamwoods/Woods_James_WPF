8ea68ade52f2c204b786cd28a64c007151cce430
jamwoods <jamwoods@fullsail.edu>
2014-07-29 17:53:06 -0600

    Final Notes on ScreenCast
    Final Notes on ScreenCast

--- Week 4 Non Graded Activites/Screencast_Functions/js/screencast_functions.js
+++ Week 4 Non Graded Activites/Screencast_Functions/js/screencast_functions.js
@@ -1,186 +1,87 @@
 /**
  * Created by James Woods
  * Origination Date 7/28/14.
- * Week 4 ScreeCast Practice
+ * Week 4 ScreenCast Practice
  */
 
-//Conditional Comparison Statement with Temp
+//Reusable-Function
 
-//Populated value in var determines whether the subsequent argument will execute.
-var temp = 82;
-if (temp>70) {goToTheBeach();
+//Functions Store stuff, specifically lines of code as opposed to variables that store values. Think of them like factories.
+//Different types of functions. Functions=Normal Functions, Procedures, Anonymous Functions
+
+//Basic Structure
+//function functionName(){//Instead of "if" we will use the keyword function. The parenthesis may be empty or they may contain a parameter
+    //code
 }
 
-var temp = 82;//In this scenario, you are trying to compare values and look for an exact match
-if (temp==70) {goToTheBeach();
-}
-
-//Nested Conditional
-
-if (sunny){
-    goToTheBeach();
-
-    if (warmWater){
-    wearNewSuit();
-}
-
-}else{
-    {
-        goToTheMovies();
-    }
-
-var oldEnough=true;
-
-//if the child is old enough, print to the console, "You can ride!"
-if(oldEnough){
-    //code to be performed if condition is true
-    console.log("You can ride the coaster!");
-}
-
-//Relational Expressions-Check the relationship between 2 variables or values
-
-var kidHeight = 30;
-var minHeight= 48;
-
-//If over 48"
-if (kidHeight>minHeight){
-}
-
-
-console.log("You can ride the coaster!");
-
-    if (kidHeight +2 > minHeight){//sneaker lift. Can also declare a variable value and add the two variables together
-
-    }
-
-    var kidHeight = 30;
-    var minHeight= 48;
-    var sneakerLift=2;
-
-    if (kidHeight + sneakerLift > minHeight){//sneaker lift. Can also declare a variable value and add the two variables together
-    }
-//if and else
-
-    var kidHeight = 30;
-    var minHeight= 48;
-
-//If under 48"
-    if (kidHeight<minHeight){
-    }
-    console.log("Need to put on a few inches")
-//But what if neither condition is met? Use the less than or equal to operator
-    if (kidHeight<=minHeight){
-    }
-//Alternative method that combines both with an else
-
-if (kidHeight>minHeight){
-}else}//removes the need for another relational operator
-    console.log("Need to put on a few inches");
-}
-
-//else if chain. Choosing between three blocks of code
-
-var estDinnerBill = prompt("How much money are you going to drop at dinner if you get everything you want?")
-console.log("estDinnerBill");
-var dailyBudget = prompt("What is your daily budget?")
-
-var dinnerBill = 52;
-var minHeight= 48;
-var wParentHeight=45;
-
-if (dinnerBill>dailyBudget){
-    alert("Hit the ride");
-}else if(kidHeight>wParentHeight) {
-    console.log("You can ride with a parent");
-}else{
-    console.log("Need o grow a little");
-}
-
-//Logical operators
-
-//&& and requires that both boolean values be true
-
-var budget = 300
-var iPhonePrice = 199.99
-var paycheck = 200;
-var wonLottery = true;
-
-if(iPhonePrice < budget && paycheck > 300 ) {//and operators only goes between Booleans
-    console.log("We can buy the phone")
-}else{
-    console.log("We can buy the phone")
+//function outptMsg(){//
+    console.log("Hello World");//You could embed variables and the calculation of those variables, so width height and W*H
 
 }
 
-//Or operator ||
+//Function Invocation-The call must be invoked in order for the function to execute. They have to be called. How do you do that. Write the name of the function and
+// follow it by the two parenthesis. function call=function invocation
 
-var budget = 100
-var iPhonePrice = 199.99
-var paycheck = 200;
-var wonLottery = true;
-
-if(iPhonePrice < budget || wonLottery) {//and operators only goes between Booleans
-    console.log("We can buy the phone")
-}else{
-    console.log("We can buy the phone")
-
-}
-//Exclusive OR Xor. Only one of those things can be true.Either on but not both. If both are true it resolved to false
-//Not Operator-Negation-true becomes false, false becomes true
-//a!=b same as !(a===b)
-//a<b is the same as !(a<b)
-//COMMON MISCONCEPTIONS
-//a===b===c is not correct for equality. Should be a===b && b===c. This is because the first expression if the wrong example resolves
-//to true. You end up saying true = c. In case of the latter, if both are true the whole thing is true.
-//Relational operators go in between pairs of objects
-//Logical operators go in between Relational Expressions or Boolean values
-//Always work in pairs, never three items at once
-
-//Ternary operators-one line if else statement that requires 3 components
-
-/*if(condition){
-    do if true;
-}else{
-    do if false;
+//Variable Scope
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
 }
 
+calcArea();//What if you had a width outside the function. Code prefers to render variables found within the function. We say the the width variable is scoped to calcArea().
+//If there is code that is outside of a function like console.log it will call the variables that are also outside of the function.
+
+//Arguments and Parameters-You need something to be slightly different every time you use it.
+
+//Parameter allow us to store and use different basic material as we need them. Not all functions have to have parameters and arguments
+
+funcName(argument1, argument2);
+function funcName(parameter1, parameter2){
+    //code
+}
+
+
+calcArea(30,20);//30 and 20 are the arguments
+function calcArea(w,h){//storage bins. order matters. 30 is in the first parameter, 20 in the second.
+    var area = w * h;
+    console.log(area);
+}
+
+
+function dogYears(age){//Parenthesis here holds parameters-storage container. The function definition
+    var dogYears = age * 7//Function call
+    console.log("Sparky is" + dogYears + "years old")
+}
+dogYears(4);//Parenthesis here holds arguments
+
+//Returning values
+
+var total = calcArea(30,20);//30 and 20 are the arguments//Whatever that function returns store it into this variable. Functions that are returning should be assigned to variables.
+
+function calcArea(w,h){//storage bins. order matters. 30 is in the first parameter, 20 in the second.
+    var area = w * h;
+    return area;
+
+}
+console.log(total);//Generally functions that return value should be assigned to variables.
+
+//Function versus Procedure
+//Functions return values and procedures do not. Functions that just have a list of things to do are procedures.
+
+//Anonymous Function-works the same way as earlier structure. Downside is that they have to be declared before they are called. So invoking should happen after function definition.
+// These are used for the sake of code brevity
+//They are created and given a name at the same time the code is executed. Also called closures
+//var functionName = function(){
+//code to run
+}
 //OR
-
-(condition) ? do if true : do if false;
-
-
-var gpa = 4.8;
-if(gpa>2.0) {
-    console.log("Congrats");
-}else{
-    console.log("Congrats");
-}*/
-
-//OR you could use a ternary
-
-(gpa > 2.0) ? console.log("Congrats") : console.log("So sorry Charlie")
-
-//Real benefit is its ability to return values
-
-var age = 11;
-var book;
-
-//If child under 10 they get book A otherwise they get B
-
-if(age<10){
-    book="A";
-}else{
-    book = "B";
-}
-
-console.log(book);
-
-//Using Ternary-It is really a choice to use ternary's or conditionals. Really large blocks of codes are better suited for traditional conditionals
-
-book = (age<10) ? "A" : "B"
-console.log(book);
-
-
-
+var calcArea = function( width, height){(
+    ///code that runs
+var area = width * height
+    return area;
+ }
+calcArea (20,30);//can return to a variable
 
 
