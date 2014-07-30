f845e0b2a3a3ba47cf3e4d741d4800d3b29fba7b
jamwoods <jamwoods@fullsail.edu>
2014-07-29 20:54:12 -0600

    Removed parseFloat as it was unnecessary
    Removed parse float as it was unnecessary

--- Week 4 Non Graded Activites/Functions_worksheet/js/functions_worksheet.js
+++ Week 4 Non Graded Activites/Functions_worksheet/js/functions_worksheet.js
@@ -9,12 +9,11 @@ circleCir(6); //Arguments presumably to be supplied by visitor input
 function circleCir(r){//function call with 1 parameter
    var circumference = 3.14 * r*2;//output of the function
    console.log("The circumference of the circle is" + " " + circumference);
-}
+};
 
 //Attack of the Bees
 circleCir(215);//Weight of animal
 function circleCir(weight){//function call with 1 parameter
-    var stings = parseFloat(weight/8.67).toFixed(2);//Output of the function limited to 2 decimal places
+    var stings =(weight/8.67).toFixed(2);//Output of the function limited to 2 decimal places
     console.log("It takes" + " " + stings + " " + "stings to kill this animal");
-}
-
+};
