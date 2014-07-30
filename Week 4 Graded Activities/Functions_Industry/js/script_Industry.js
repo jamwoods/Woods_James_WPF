b71872983b234dc0bdf4fc01ac63fb7fd5f6123c
jamwoods <jamwoods@fullsail.edu>
2014-07-30 15:44:08 -0600

    Finished Industry Script
    Finished Industry Script and added comments

--- Week 4 Graded Activities/Functions_Industry/js/script_Industry.js
+++ Week 4 Graded Activities/Functions_Industry/js/script_Industry.js
@@ -10,57 +10,57 @@
 
     Each of the following requirements must exist in at least one of the 3 subprojects:
     A ternary
-An else if
+
     An anonymous function
     A normal "named" function
     An expression with two arithmetic operators
-A function with three parameters.
+
     At least one logical operator
 */
 //Calculating total cost for training delivery
 
-var requestedDaysPrompt =  prompt("How many days of training would you like?"); //User is prompted for their quarterly quality score
+var requestedDaysPrompt =  prompt("How many days of training would you like?"); //User is prompted for the number of training days they would like
 if (requestedDaysPrompt === ""){
     requestedDaysPrompt = prompt("How many days of training would you like? Do not leave blank.","0");//Re-prompting if no value is entered
 }
-var requestedDays = (requestedDaysPrompt); //The number from the prompt is returned with a decimal place if necessary, e.g. 9.2
+var requestedDays = (requestedDaysPrompt); //In case we run into people who are requesting partial days. If so add float here.
 console.log(requestedDays);
 
-var formatPrompt =  prompt("Would you prefer an onsite, virtual or public delivery method?"); //User is prompted for their quarterly quality score
+var formatPrompt =  prompt("Would you prefer an onsite, virtual or public delivery method?"); //User is prompted for their delivery method
 if (formatPrompt === ""){
     formatPrompt = prompt("Would you prefer an onsite, virtual or public delivery method? Do not leave blank.");//Re-prompting if no value is entered
 }
 
 if(formatPrompt == "onsite"){
-    var cost =5300;
+    var cost =5300;//Standard cost for onsite per day
 }else if(formatPrompt == "virtual"){
-    var cost = 4800;
+    var cost = 4800;//Standard cost for virtual per day
 }else{
-    var cost = 900;
+    var cost = 900;//Standard cost for public per day, the only remaining option.
 }
 console.log(cost);
 
-var discountPrompt =  prompt("do you have a discount code?"); //User is prompted for their quarterly quality score
+var discountPrompt =  prompt("do you have a discount code?"); //User is prompted for their discount code if applicable. Only accounting for two codes, dc10 and dc20
 if (discountPrompt === ""){
     discountPrompt = prompt("do you have a discount code? Do not leave blank.");//Re-prompting if no value is entered
 }
 
 if(discountPrompt == "dc10"){
-    var discount = .10;
+    var discount = .10;//10% off list
 }else if(discountPrompt == "dc20") {
-    var discount = .20;
+    var discount = .20;//20% off list
 }else{
     var discount = 0;
 }
 console.log(discount);
 
-var training = productDetail(requestedDays,cost,discount); //Arguments presumably to be supplied by visitor input;
-function productDetail(requestedDays,cost,discount){//function call with 1 parameter
-    var net = requestedDays*cost-requestedDays*cost*discount;//output of the function
+productDetail(requestedDays,cost,discount);//function call pulling data from user inputs
+function productDetail(requestedDays,cost,discount){//function call with 3 parameters.
+    var net = requestedDays*cost-requestedDays*cost*discount;//output of the function. Calculation takes into account cost per day and discount if any.
     console.log("Your total training cost will be $" + net + ".");
 };
 
-productDetail(requestedDays,cost,discount);
+productDetail(5,900,.10);//Second booking with different values. Called the function twice to test
 /*
 //Hours worked in a week
 alert ("Let's see what percentage of your bonus you will be awarded this quarter.");
