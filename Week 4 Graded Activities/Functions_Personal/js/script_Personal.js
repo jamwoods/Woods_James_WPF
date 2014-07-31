6812aedf20b623c8e1d56373f52dd4f594633193
jamwoods <jamwoods@fullsail.edu>
2014-07-30 18:45:07 -0600

    Added Comments
    Added Comments

--- Week 4 Graded Activities/Functions_Personal/js/script_Personal.js
+++ Week 4 Graded Activities/Functions_Personal/js/script_Personal.js
@@ -5,18 +5,7 @@
  */
 
 
-/*Use a function as part of your code in each sub-project. This function should have at least two parameters and return a value.
-
-
- Each of the following requirements must exist in at least one of the 3 subprojects:
-
- A ternary
- An anonymous function
- A normal "named" function
- An expression with two arithmetic operators
- At least one logical operator
- */
-//Calculating total cost for training delivery
+//Calculating correct food quantity for Bella
 
 
 var weight =  prompt("How much does Bella weight?"); //User is prompted for weight of the dog
@@ -24,7 +13,7 @@ if (weight === ""){
     weight = prompt("How much does Bella weight? Do not leave blank.");//Re-prompting if no value is entered
 }
 console.log(weight)
-var cups =  prompt("How many cups do you feed Bella a day?"); //User is prompted for weight of the dog
+var cups =  prompt("How many cups do you feed Bella a day?"); //User is prompted for current cup count
 if (cups === ""){
     cups = prompt("How many cups do you feed Bella a day? Do not leave blank.");//Re-prompting if no value is entered
 }
@@ -33,19 +22,19 @@ console.log(cups)
  var dogFood = function(weight,cups){
 //Code the function runs
  if(weight >= 40){
- var properFeeding = cups - cups * .25;//10% off list
+ var properFeeding = cups - cups * .25;//10% off list//If overweight need to reduce cup count
      console.log(properFeeding);
- }else if(weight <=39 && weight>=32) {
+ }else if(weight <=39 && weight>=32) {//If on target weight cup count remains the same
     var properFeeding = cups;//20% off list
      console.log(properFeeding);
- }else if(weight <32 && weight>=28) {
+ }else if(weight <32 && weight>=28) {//If under weight cup count is reduced
     var properFeeding = cups + cups * .25;//20% off list
      console.log(properFeeding);
  }else{
-    alert("Take your dog to the vet!")
+    alert("Take your dog to the vet!")//If drastically under weight see the vet.
  }
 
  }
 
-dogFood(weight,cups);
+dogFood(weight,cups);//Invoking the function
 
