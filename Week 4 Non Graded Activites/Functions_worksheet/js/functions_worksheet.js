387c0a5c9e8b2bab3a0331ba86256e8a1a70687b
jamwoods <jamwoods@fullsail.edu>
2014-07-30 22:00:04 -0600

    Corrected function reference
    Function reference for the bee sting activity was referring to the
    circumference function. Had to correct.

--- Week 4 Non Graded Activites/Functions_worksheet/js/functions_worksheet.js
+++ Week 4 Non Graded Activites/Functions_worksheet/js/functions_worksheet.js
@@ -5,15 +5,15 @@
  */
 
 //Circumference of a circle
-circleCir(6); //Arguments presumably to be supplied by visitor input
+circleCir(6); //radius in inches
 function circleCir(r){//function call with 1 parameter
    var circumference = 3.14 * r*2;//output of the function
-   console.log("The circumference of the circle is" + " " + circumference);
+   console.log("The circumference of the circle is" + " " + circumference + " inches");
 };
 
 //Attack of the Bees
-circleCir(215);//Weight of animal
-function circleCir(weight){//function call with 1 parameter
-    var stings =(weight/8.67).toFixed(2);//Output of the function limited to 2 decimal places
+wildBees(215);//Weight of animal
+function wildBees(weight){//function call with 1 parameter
+    var stings =(weight/8.67).toFixed(1);//Output of the function limited to 1 decimal places
     console.log("It takes" + " " + stings + " " + "stings to kill this animal");
 };
